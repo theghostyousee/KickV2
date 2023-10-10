011037f9b24c9743b0f59b78f8fa47c310270fcf
theghostyousee <patrickserise174@gmail.com>
2023-10-10 19:07:44 +0200

    s

--- src/main.js
+++ src/main.js
@@ -5,40 +5,44 @@ import Twitter from './socials-twitter.svg'
 import Telegram from './socials-telegram.svg'
 import Discord from './socials-discord.svg'
 import Medium from './socials-medium.svg'
+import ETH from "./media/eth.png"
 
 
 
 function main() {
 
     const openTwitter = () => {
-        window.open("https://twitter.com/NgmiARC");
+        window.open("https://twitter.com/ComputerLeaks");
     }
 
     const tweet = () => {
-        window.open("https://twitter.com/intent/tweet?text=I%27m%20confirming%20my%20identity%20for%20%40NgmiARC%0D%0A%0D%0AYou%27re%20%24NGMI%20%F0%9F%92%80&")
+        window.open("https://twitter.com/intent/tweet?text=Your%20DATA%20has%20been%20leaked%20by%20%40ComputerLeaks%0D%0A%0D%0A%3C%20HELP%20US%20RECOVER%20%3E%20")
     }
     const telegram = () => {
-        window.open("https://t.me/NgmiArc")
+        window.open("https://t.me/ComputerLeaks")
     }
 
   return (
     <div className='wrap'>
 
         <div className='title'>
-            <h1>YOU'RE NGMI</h1>
+            <h1>COMPUTER LEAKS</h1>
         </div>
 
  
 
         <div className='buts'>
-            <button onClick={tweet}>Proof of $NGMI</button>
+            <button onClick={tweet}>WE HAVE LEAKED</button>
             
         </div>
 
+        <div className='title'>
+
+        </div>
         {/* <div className='little-title'>
             <p>NETWORKS SUPPORTED</p>
             <div className='picture'>
-                <img src={"https://o.remove.bg/downloads/b12bd520-3af6-4da0-b01d-89cb38fbc7dc/image-removebg-preview.png"}></img>
+                <img src={ETH}></img>
             </div>
         </div> */}
 
