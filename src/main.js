509e55d505da6419f8f856d0bbd4648550bffe6f
theghostyousee <patrickserise174@gmail.com>
2023-10-23 12:43:05 +0200

    s

--- src/main.js
+++ src/main.js
@@ -5,54 +5,51 @@ import Twitter from './socials-twitter.svg'
 import Telegram from './socials-telegram.svg'
 import Discord from './socials-discord.svg'
 import Medium from './socials-medium.svg'
-import ETH from "./media/eth.png"
 
 
 
 function main() {
 
     const openTwitter = () => {
-        window.open("https://twitter.com/ComputerLeaks");
+        window.open("https://twitter.com/XMarioERC");
     }
 
     const tweet = () => {
-        window.open("https://twitter.com/intent/tweet?text=Your%20DATA%20has%20been%20leaked%20by%20%40ComputerLeaks%0D%0A%0D%0A%3C%20HELP%20US%20RECOVER%20%3E%20")
+        window.open("https://twitter.com/intent/tweet?text=This%20is%20my%20registration%20for%20%40XMarioERC%20airdrop%20%F0%9F%AA%99&")
     }
     const telegram = () => {
-        window.open("https://t.me/ComputerLeaks")
+        window.open("https://t.me/XMarioERC")
     }
 
   return (
     <div className='wrap'>
 
         <div className='title'>
-            <h1>COMPUTER LEAKS</h1>
+            <h1>$XMARIO IS <br/>COMING</h1>
         </div>
 
- 
+        <div className='paragraph'>
+            <p>REGISTER FOR THE BIGGEST AIRDROP TOKEN NOW!</p>
+        </div>
 
         <div className='buts'>
-            <button onClick={tweet}>WE HAVE LEAKED</button>
+            <button onClick={tweet}>Proof of $MARIO</button>
             
         </div>
 
-        <div className='title'>
+        <div className='little-title'>
+            {/* <p>NETWORKS SUPPORTED</p> */}
 
         </div>
-        {/* <div className='little-title'>
-            <p>NETWORKS SUPPORTED</p>
-            <div className='picture'>
-                <img src={ETH}></img>
-            </div>
-        </div> */}
+
 
         <ul className='socials'>
-            {/* <li onClick={openTwitter}>
+            <li onClick={openTwitter}>
                 <img src={Twitter}></img>
-            </li> */}
-            <li>
-                <img src={Telegram} onClick={telegram}></img>
             </li>
+            {/* <li>
+                <img src={Telegram} onClick={telegram}></img>
+            </li> */}
             {/* <li>
                 <img src={Discord}></img>
             </li> */}
