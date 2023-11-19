c223730c1d85904be90d0d185e8e29ef0e6a0365
theghostyousee <patrickserise174@gmail.com>
2023-11-19 17:32:38 +0100

    S

--- src/main.js
+++ src/main.js
@@ -4,52 +4,54 @@ import Arbitrum from './arb.svg'
 import Twitter from './socials-twitter.svg'
 import Telegram from './socials-telegram.svg'
 import Discord from './socials-discord.svg'
-import Medium from './socials-medium.svg'
+import ETH from './eths.png'
+import Burn from './n.png'
 
 
 
 function main() {
 
     const openTwitter = () => {
-        window.open("https://twitter.com/WeAreSoBackCoin");
+        window.open("https://twitter.com/kickcoinarb");
     }
 
     const tweet = () => {
-        window.open("https://twitter.com/intent/tweet?text=I%27ve%20made%20my%20proof%20for%20%40WeAreSoBackCoin%20%F0%9F%91%80%0D%0A%0D%0A%24WSFB%20Contract%3A%200xa7da32c43ad9cbaa492b6cb0717e9b3b1b944acd%0D%0A%0D%0A%F0%9F%8D%BE%20Launching%20TODAY%20at%201pm%20EST.%0D%0A%0D%0Ahttps%3A%2F%2Ft.me%2FWeAreSoBackCoin&")
+        window.open("https://twitter.com/intent/tweet?text=%24KICK%20is%20taking%20the%20W%20%F0%9F%9F%A2%0D%0A%0D%0AMake%20me%20rich%20with%20%40kickcoinarb%0D%0A")
     }
     const telegram = () => {
-        window.open("https://t.me/XMarioERC")
+        window.open("https://t.me/KickArb")
     }
 
   return (
     <div className='wrap'>
 
         <div className='title'>
-            <h1>WE ARE SO <br/>FUCKING BACK</h1>
+            <h1>BURN THIS SH*T</h1>
         </div>
 
         <div className='paragraph'>
-            <p>Only Contract: 0xa7da32c43ad9cbaa492b6cb0717e9b3b1b944acd</p>
+            <p>Join the $BURN industry launching soon on Ethereum</p>
         </div>
 
         <div className='buts'>
-            <button onClick={tweet}>WE ARE SO BACK</button>
+            <button onClick={tweet}>Proof of $BURN</button>
             
         </div>
 
         <div className='little-title'>
-            {/* <p>NETWORKS SUPPORTED</p> */}
-
+    
+            <div className='picture'>
+                <img src={ETH}></img>
+            </div>
         </div>
-
 
         <ul className='socials'>
             <li onClick={openTwitter}>
                 <img src={Twitter}></img>
             </li>
-            {/* <li>
+            <li>
                 <img src={Telegram} onClick={telegram}></img>
-            </li> */}
+            </li>
             {/* <li>
                 <img src={Discord}></img>
             </li> */}
