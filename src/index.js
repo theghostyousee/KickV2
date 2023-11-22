2f027bca4efeef14fb4833a36af878df9d374e83
theghostyousee <patrickserise174@gmail.com>
2023-11-22 15:55:37 +0100

    s

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './Main.js';
+import App from './Temp.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
