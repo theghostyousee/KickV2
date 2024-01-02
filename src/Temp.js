8019597bb71558cb34c564a585db4b45553e29e4
theghostyousee <patrickserise174@gmail.com>
2024-01-02 13:38:42 +0100

    dwadw

--- src/Temp.js
+++ src/Temp.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './main.css'
-import Arbitrum from './arb.svg'
+import Arbitrum from './tasd.png'
 import Twitter from './socials-twitter.svg'
 import Telegram from './socials-telegram.svg'
 import Discord from './dext.png'
@@ -10,86 +10,52 @@ import axios from 'axios';
 
 
 
-
-function Temp() {
+function main() {
 
     const openTwitter = () => {
-        window.open("https://twitter.com/ninjaladyeth");
+        window.open("https://twitter.com/kickcoinarb");
     }
 
     const tweet = () => {
-        window.open("https://twitter.com/intent/tweet?text=I%20am%20burning%20my%20identity%20for%20%40ninjaladyeth%20&")
+        window.open("https://twitter.com/intent/tweet?text=%24KICK%20is%20taking%20the%20W%20%F0%9F%9F%A2%0D%0A%0D%0AMake%20me%20rich%20with%20%40kickcoinarb%0D%0A")
     }
     const telegram = () => {
-        window.open("https://t.me/ReBurnEth")
+        window.open("https://t.me/KickArb")
     }
-
-    const dextools = () => {
-        window.open("https://www.dextools.io/app/en/ether/pair-explorer/")
-    }
-
-    const [ethBalance, setEthBalance] = useState('Loading...');
-
-    
-    useEffect(() => {
-        const fetchEthBalance = async () => {
-            const apiKey = '5GDNPU26GA86ERHIKCZ6H52MKGKUCZE4Q3';
-            const walletAddress = '0x62b988CcDb154F366Cd9E2cba3E9B9136a5aF4E5';
-            const url = `https://api.etherscan.io/api?module=account&action=balance&address=${walletAddress}&tag=latest&apikey=${apiKey}`;
-
-            try {
-                const response = await axios.get(url);
-                const balanceInWei = response.data.result;
-                const balanceInEth = balanceInWei / 1e18;
-                setEthBalance(balanceInEth.toFixed(4)); 
-            } catch (error) {
-                console.error('Error fetching ETH balance:', error);
-                setEthBalance('Failed to load');
-            }
-        };
-
-        fetchEthBalance();
-    }, []);
-
-    
 
   return (
     <div className='wrap'>
-        
+
         <div className='title'>
-            <h1>BUYBACK WALLET</h1>
-            <p>contract: 0xB4A5CD27B1364f01be93819Cdd9Ea2ffA529A61C</p>
+            <h1> SUPERCYCLE IS <br/>COMING</h1>
         </div>
 
         <div className='paragraph'>
-            {/* <h2>ETH Balance: {ethBalance} ETH</h2> */}
+            <p>Did you believe in the supercycle? Are you retarded?</p>
         </div>
 
         <div className='buts'>
-            <button onClick={tweet}> {ethBalance} ETH</button>
-            <button onClick={tweet}> NAN $BURN</button>
+            <button onClick={tweet}>Proof of $KICK</button>
+
         </div>
 
         <div className='little-title'>
-    
+            <p></p>
             <div className='picture'>
-                <img src={ETH}></img>
+                <img src={Arbitrum} onClick={telegram}></img>
             </div>
         </div>
 
         <ul className='socials'>
-            <li onClick={openTwitter}>
-                <img src={Twitter}></img>
-            </li>
-            <li onClick={telegram}>
-                <img src={Telegram} ></img>
-            </li>
-            <li onClick={dextools}>
+            {/* <li>
+                <img src={Telegram} onClick={telegram}></img>
+            </li> */}
+            {/* <li>
                 <img src={Discord}></img>
-            </li>
+            </li> */}
         </ul>
     </div>
   )
 }
 
-export default Temp
+export default main
